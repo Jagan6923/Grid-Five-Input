d5ad611a7f897a1d87ba838c49fa4b8f3f9f308c
Jeyaraman S <137631905+Jagan6923@users.noreply.github.com>
2024-02-20 22:17:57 +0530

    Label work finished

--- formapp/views.py
+++ formapp/views.py
@@ -23,13 +23,16 @@ def validate_project_name(request):
         return JsonResponse({'exists': exists})
     return JsonResponse({'exists': False})
 
-
 def save_draggable_input(request):
     if request.method == 'POST':
         data = request.POST.getlist('data[]')
         x_axis = request.POST.getlist('x_axis[]')
         y_axis = request.POST.getlist('y_axis[]')
         input_field_ids = request.POST.getlist('input_field_id[]')
+        label_names = request.POST.getlist('label_name[]')
+        label_ids = request.POST.getlist('label_id[]')
+        label_x_axis = request.POST.getlist('label_x_axis[]')  # New field for label x-axis
+        label_y_axis = request.POST.getlist('label_y_axis[]')  # New field for label y-axis
 
         # Loop through the received data and save each draggable input field
         for i in range(len(data)):
@@ -37,7 +40,11 @@ def save_draggable_input(request):
                 data=data[i],
                 x_axis=x_axis[i],
                 y_axis=y_axis[i],
-                input_field_id=input_field_ids[i]
+                input_field_id=input_field_ids[i],
+                label_name=label_names[i],
+                label_id=label_ids[i],
+                label_x_axis=label_x_axis[i],  # Include label x-axis value
+                label_y_axis=label_y_axis[i]   # Include label y-axis value
             )
             draggable_input.save()
 
